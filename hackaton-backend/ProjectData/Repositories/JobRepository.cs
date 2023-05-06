0a21c57bbfaf4737bcd220b2f7454d679a61dabb
TheSlayerBrine <cgamer788@gmail.com>
2023-05-06 16:17:43 +0300

    JobRepository

---
+++ hackaton-backend/ProjectData/Repositories/JobRepository.cs
@@ -0,0 +1,32 @@
+﻿using hackatonBackend.ProjectData.Entities;
+using hackatonBackend.ProjectData.Infrastructure.Context;
+using hackatonBackend.ProjectData.Infrastructure.Repository;
+using hackatonBackend.ProjectData.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace hackatonBackend.ProjectData.Repositories
+{
+    public class JobRepository : Repository<Job>, IJobRepository
+    {
+        private readonly IAppDbContext dbContext;
+        public JobRepository(IAppDbContext dbContext) : base ((DbContext)dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+        public Job GetJobById( int id) 
+        { 
+            return dbContext.Jobs
+                .FirstOrDefault(j => j.Id == id);
+        }
+        public Job GetJobByName( string name ) 
+        {
+            return dbContext.Jobs
+                .FirstOrDefault(j => j.Name == name);
+        }
+        public Job GetJobByCompanyId ( int companyId ) 
+        {
+            return dbContext.Jobs
+                .FirstOrDefault(j => j.CompanyId == companyId);
+        }
+    }
+}
